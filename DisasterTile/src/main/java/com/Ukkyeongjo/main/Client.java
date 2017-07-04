4b10ed04dd51ffc14065a6ce6fc8aed5b69ae137
DSMGun <geon0250@gmail.com>
2017-07-04 11:10:40 +0900

    Modify server verticle

--- DisasterTile/src/main/java/com/Ukkyeongjo/main/Client.java
+++ DisasterTile/src/main/java/com/Ukkyeongjo/main/Client.java
@@ -1,12 +1,26 @@
 package com.Ukkyeongjo.main;
 
-import io.vertx.core.Vertx;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpClient;
 
-public class Client {
-	// Convenience method so you can run it in your IDE
-	public static void main(String[] args) {
-		Runner.runExample(Client.class);
-		Vertx vertx = Vertx.vertx();
-		vertx.deployVerticle(new Verticle());
-	}
+public class Client extends AbstractVerticle {
+
+  // Convenience method so you can run it in your IDE
+  public static void main(String[] args) {
+    Runner.runExample(Client.class);
+  }
+
+  @Override
+  public void start() throws Exception {
+    HttpClient client = vertx.createHttpClient();
+
+    client.websocket(8080, "localhost", "/ws-game", websocket -> {
+      websocket.handler(data -> {
+        System.out.println("Received data " + data.toString("ISO-8859-1"));
+        client.close();
+      });
+      websocket.writeBinaryMessage(Buffer.buffer("Hello worlddsaf"));
+    });
+  }
 }
