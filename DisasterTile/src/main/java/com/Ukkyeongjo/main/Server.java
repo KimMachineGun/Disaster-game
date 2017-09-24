43ad3b2f60ecf0c6f7ac56df2aaa21a2ac861ad2
unknown <geon0250@gmail.com>
2017-09-24 22:30:28 +0900

    Create Login, Logout, Register method

--- DisasterTile/src/main/java/com/Ukkyeongjo/main/Server.java
+++ DisasterTile/src/main/java/com/Ukkyeongjo/main/Server.java
@@ -1,23 +1,19 @@
 package com.Ukkyeongjo.main;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import io.netty.handler.codec.http.HttpResponse;
-import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServerResponse;
-import io.vertx.core.http.ServerWebSocket;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.Session;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.CookieHandler;
+import io.vertx.ext.web.handler.SessionHandler;
 import io.vertx.ext.web.handler.StaticHandler;
-
-import java.sql.*;
+import io.vertx.ext.web.sstore.LocalSessionStore;
 
 public class Server {
 	static Vertx vertx = null;
@@ -39,15 +35,16 @@ public class Server {
 		Router router = Router.router(vertx);
 		
 		router.route().handler(BodyHandler.create());
+	    router.route().handler(CookieHandler.create());
+	    router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx)));
 		
 		// Static 파일 핸들러
 		router.route("/static/*").handler(StaticHandler.create("../front/"));
 		
 		// 메인페이지 라우팅
 		router.get("/").handler(ctx -> {
-			HttpServerResponse response = ctx.response();
-			  
-			response.sendFile("../front/main_page.html");
+			HttpServerResponse res = ctx.response();
+			res.sendFile("../front/main_page.html");
 		});
 		
 		router.post("/register").handler(ctx -> {
@@ -57,24 +54,23 @@ public class Server {
 			String tPw = json.getString("pw");
 			String tRePw = json.getString("re-pw");
 			if(!tPw.equals(tRePw)) {
-				res.setStatusCode(400).setStatusMessage("It is not same").end();
+				res.setStatusCode(400).setStatusMessage("It's not same").end();
 			} else {
 				String sql = "INSERT INTO USERS(ID,PW) VALUES('" + tId + "','" + tPw + "');";
-				
-				System.out.println("ID : " + tId);
-				System.out.println("PW : " + tPw);
-				
 				try {
 					DB.statement.executeUpdate(sql);
+					System.out.println("REGISTER SUCCESS : " + tId);
+					res.setStatusCode(200).end();
 				} catch (SQLException e) {
+					System.out.println("REGISTER FAILED");
 					res.setStatusCode(400).setStatusMessage("Register Failed").end();
 				}
-				res.end();
 			}
 		});
 		
 		router.post("/login").handler(ctx -> {
 			HttpServerResponse res = ctx.response();
+			Session session = ctx.session();
 			JsonObject json = ctx.getBodyAsJson();
 			
 			String tId = json.getString("id");
@@ -85,17 +81,44 @@ public class Server {
 			try {
 				pstmt = DB.conn.prepareStatement(sql);
 
-				pstmt.setString(1,tId);
+				pstmt.setString(1, tId);
 				pstmt.setString(2, tPw);
 
 				ResultSet rs = pstmt.executeQuery();
 
-				System.out.println(rs.getString("ID"));
+				System.out.println("LOGIN SUCEESS : " + rs.getString("ID"));
+
+				session.put("user", tId);
+				res.setStatusCode(200).end();
 			} catch (SQLException e) {
-				System.out.println("로그인 실패");
+				System.out.println("LOGIN FAILED");
 				res.setStatusCode(400).end();
 			}
-			res.setStatusCode(200).end();
+		});
+		
+		router.get("/logout").handler(ctx -> {
+			HttpServerResponse res = ctx.response();
+			Session session = ctx.session();
+			
+			if(session.data().containsKey("user") == true) {
+				session.remove("user");
+				res.setStatusCode(200).end();
+			} else {
+				res.setStatusCode(400).end();
+			}
+			
+		});
+		
+		router.get("/session").handler(ctx -> {
+			HttpServerResponse res = ctx.response();
+			Session session = ctx.session();
+			
+			if(session.data().containsKey("user") == true) {
+				System.out.println("USER ID : " + session.get("user").toString());
+				res.setStatusCode(200).end();
+			} else {
+				res.setStatusCode(400).end();
+			}
 		});
 		
 		// 포트 8080
