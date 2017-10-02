1a0401d6ce8911852a2bb701f9feea6740656b7d
Kyungwook Cho <amisay5256@naver.com>
2017-10-02 17:10:39 +0900

    make Tiles to square size
    makeTilesToSquare is called at onload and onresize
    makeTilesTo Square makes tiles' width:height to 1:1

--- front/js/newGame.js
+++ front/js/newGame.js
@@ -4,3 +4,31 @@ for(var i = 0; i < tiles.length; i++)
 {
     tiles[i].innerHTML = i;
 }
+
+var body = document.getElementById("body");
+var width, height;
+
+//setInterval
+//(
+//    function()
+//    {
+//        console.log("setInterval");
+//    }
+//);
+
+onresize = function()
+{
+    makeTilesToSquare();
+}
+
+function makeTilesToSquare()
+{
+    var left = document.getElementsByTagName("left")[0];
+    var main = document.getElementsByTagName("main")[0];
+    var bottom = document.getElementsByTagName("bottom")[0];
+    
+    main.style.height = ((main.offsetWidth - 64) / 2) + 64 + "px";
+    bottom.style.height = left.offsetHeight - main.offsetHeight + "px";
+}
+
+makeTilesToSquare();
