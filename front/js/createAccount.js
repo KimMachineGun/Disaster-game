a731b6e4a76aef952a34796e13c5d43c9e6d9f93
Kyungwook Cho <amisay5256@naver.com>
2017-10-14 21:45:55 +0900

    Set CreateAccount href

--- front/js/createAccount.js
+++ front/js/createAccount.js
@@ -1,18 +1,33 @@
-    $("#sign-up").on("click", function() {
-    let jsonData = JSON.stringify({
-        "id": $("#username").val(),
-        "pw": $("#password").val(),
-        "re-pw": $("#re-password").val()
-    });
-    $.ajax({
-        url: '/register',
-        type : 'post',
-        data : jsonData,
-        success : function() {
-            window.location.href = "/";
-        },
-        error : function() {
-            alert("Register Failed");
+document.getElementById("sign-up").onclick = function()
+{
+    let jsonData = JSON.stringify
+    (
+        {
+            "id": $("#username").val(),
+            "pw": $("#password").val(),
+            "re-pw": $("#re-password").val()
         }
-    }); 
-});
+    );
+    
+    $.ajax
+    (
+        {
+            url: '/register',
+            type : 'post',
+            data : jsonData,
+            success : function()
+            {
+                window.location.href = "/";
+            },
+            error : function()
+            {
+                alert("Register Failed");
+            }
+        }
+    );
+}
+
+document.getElementById("back").onclick = function()
+{
+    window.location.href = "/";
+}
