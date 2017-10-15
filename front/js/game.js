abaea4c09fbd3c090b8145c63f560a619eaf3c74
Kyungwook Cho <amisay5256@naver.com>
2017-10-15 20:36:39 +0900

    Update WebSocket Address

--- front/js/game.js
+++ front/js/game.js
@@ -121,7 +121,7 @@ reader.onload = function(event)
 
 if (window.WebSocket)
 {
-    socket = new WebSocket("ws://52.79.133.54/game-ws");
+    socket = new WebSocket("ws://13.124.89.246/game-ws");
 
     socket.onmessage = function (event)
     {
