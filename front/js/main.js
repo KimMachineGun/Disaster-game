182c413462ed7abd620bffccb80d9094444ab341
Kyungwook Cho <amisay5256@naver.com>
2017-10-15 10:25:38 +0900

    Add Matching

--- front/js/main.js
+++ front/js/main.js
@@ -1,73 +1,66 @@
-var socket;
-const reader = new FileReader();
-
-reader.onload = function(event)
-{
-		let temp = JSON.parse(reader.result);
-		console.log(temp);
-		if(temp.status == 'matched')
-        {
-            window.location.href = '/game';
-        }
-};	
-
-if (window.WebSocket)
-{
-    socket = new WebSocket("ws://52.79.133.54/game-ws");
-
-    socket.onmessage = function (event)
-    {
-        reader.readAsText(event.data);
-    };
-
-    socket.onopen = function (event)
-    {
-        alert("Server On");
-		send(JSON.stringify
-            (
-                {
-                    "status" : "in-game",
-				    "code" : "connected"
-                }      
-            )
-        );
-    };
-
-    socket.onclose = function (event)
-    {
-        alert("Server Closed");
-    };
-}
-
-else
-{
-    alert("Use Different Browser");
-}
-
-
-function send(message)
-{
-    if (socket.readyState == WebSocket.OPEN)
-    {
-        socket.send(message);
-    }
-    
-    else
-    {
-        alert("WebSocket Closed");
-    }
-}
+//var socket;
+//const reader = new FileReader();
+//
+//reader.onload = function(event)
+//{
+//		let temp = JSON.parse(reader.result);
+//		console.log(temp);
+//		if(temp.status == 'matched')
+//        {
+//            window.location.href = '/game';
+//        }
+//};	
+//
+//if (window.WebSocket)
+//{
+//    socket = new WebSocket("ws://52.79.133.54/game-ws");
+//
+//    socket.onmessage = function (event)
+//    {
+//        reader.readAsText(event.data);
+//    };
+//
+//    socket.onopen = function (event)
+//    {
+//        alert("Server On");
+//		send(JSON.stringify
+//            (
+//                {
+//                    "status" : "in-game",
+//				    "code" : "connected"
+//                }      
+//            )
+//        );
+//    };
+//
+//    socket.onclose = function (event)
+//    {
+//        alert("Server Closed");
+//    };
+//}
+//
+//else
+//{
+//    alert("Use Different Browser");
+//}
+//
+//
+//function send(message)
+//{
+//    if (socket.readyState == WebSocket.OPEN)
+//    {
+//        socket.send(message);
+//    }
+//    
+//    else
+//    {
+//        alert("WebSocket Closed");
+//    }
+//}
 
 document.getElementById("start").onclick = function()
 {
-    let jsonData = JSON.stringify
-    (
-        {
-            "status": "matching"
-        }
-    );
-    
-    send(jsonData);
+    window.location.href = '/matching';
 }
 
 document.getElementById("how-to-play").onclick = function()
---
+++ front/js/matching.js
@@ -0,0 +1,83 @@
+var socket;
+const reader = new FileReader();
+
+reader.onload = function(event)
+{
+		let temp = JSON.parse(reader.result);
+		console.log(temp);
+		if(temp.status == 'matched')
+        {
+            window.location.href = '/game';
+        }
+};	
+
+if (window.WebSocket)
+{
+    socket = new WebSocket("ws://52.79.133.54/game-ws");
+
+    socket.onmessage = function (event)
+    {
+        reader.readAsText(event.data);
+    };
+
+    socket.onopen = function (event)
+    {
+        alert("Server On");
+		send(JSON.stringify
+            (
+                {
+                    "status" : "in-game",
+				    "code" : "connected"
+                }      
+            )
+        );
+    };
+
+    socket.onclose = function (event)
+    {
+        alert("Server Closed");
+    };
+}
+
+else
+{
+    alert("Use Different Browser");
+}
+
+
+function send(message)
+{
+    if (socket.readyState == WebSocket.OPEN)
+    {
+        socket.send(message);
+    }
+    
+    else
+    {
+        alert("WebSocket Closed");
+    }
+}
+
+document.getElementById("cancel").onclick = function()
+{
+    var jsonData = JSON.stringify
+    (
+        {
+            "status": "matchingCancel"
+        }
+    );
+    
+    send(jsonData);
+    window.location.href = '/';
+}
+
+// onload
+
+var jsonData = JSON.stringify
+(
+    {
+        "status": "matching"
+    }
+);
+
+send(jsonData);
