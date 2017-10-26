c55285aa51146a62c9dd96a8c620f35a4f71f3c4
unknown <geon0250@gmail.com>
2017-10-26 22:04:42 +0900

    Fixed error

--- DisasterTile/front/js/main.js
+++ DisasterTile/front/js/main.js
@@ -1,86 +1,67 @@
 var socket;
 var reader = new FileReader();
 
-reader.onload = function(event)
-{
-		let temp = JSON.parse(reader.result);
-		console.log(temp);
-		if(temp.status == 'matched')
-        {
-            window.location.href = '/game';
-        }
+reader.onload = function (event) {
+    let temp = JSON.parse(reader.result);
+    console.log(temp);
+    if (temp.status == 'matched') {
+        window.location.href = '/game';
+    }
 };
 
-if (window.WebSocket)
-{
+if (window.WebSocket) {
     socket = new WebSocket("ws://13.124.89.246:8090/game-ws");
 
-    socket.onmessage = function (event)
-    {
-			reader = new FileReader();
-      reader.readAsText(event.data);
+    socket.onmessage = function (event) {
+        console.log(event);
+        reader = new FileReader();
+        reader.readAsText(event.data);
     };
 
-    socket.onopen = function (event)
-    {
+    socket.onopen = function (event) {
     };
 
-    socket.onclose = function (event)
-    {
+    socket.onclose = function (event) {
     };
 }
 
-else
-{
+else {
 }
 
 
-function send(message)
-{
-    if (socket.readyState == WebSocket.OPEN)
-    {
+function send(message) {
+    if (socket.readyState == WebSocket.OPEN) {
         socket.send(message);
     }
 
-    else
-    {
+    else {
         alert("WebSocket Closed");
     }
 }
 
-document.getElementById("start").onclick = function()
-{
-    var jsonData = JSON.stringify
-    (
-        {
+document.getElementById("start").onclick = function () {
+    var jsonData = JSON.stringify({
             "status": "matching"
-        }
-    );
+    });
 
     send(jsonData);
     document.getElementById("matchingCover").style.display = "block";
 }
 
-document.getElementById("how-to-play").onclick = function()
-{
+document.getElementById("how-to-play").onclick = function () {
     window.location.href = '/howtoplay';
 }
 
-document.getElementById("ranking").onclick = function()
-{
+document.getElementById("ranking").onclick = function () {
     window.location.href = '/ranking';
 }
 
 // cover
 
-document.getElementById("cancel").onclick = function()
-{
-    var jsonData = JSON.stringify
-    (
-        {
+document.getElementById("cancel").onclick = function () {
+    var jsonData = JSON.stringify({
             "status": "matchingCancel"
-        }
-    );
+    });
 
     send(jsonData);
     document.getElementById("matchingCover").style.display = "none";
